3174d28aceb428c0e5c9724972ec4372588f2899
Taylor-Stephenson <taylor.stephenson@ctx.edu>
2018-10-15 20:25:54 +0000

    Turning in Assignment 3

--- PA3-2.s
+++ PA3-2.s
@@ -31,37 +31,60 @@ main:
     LDR R1, =select  @Address of var select
     LDR R1, [R1]     @Value of select in R1
 
-    @Your modifications will begin at this point
+	@Your modifications will begin at this point
 
-    CMP R1, #1       @Check for "peanuts"
-    BEQ _peanut      @If user entered 1 goto _peanuts
-    CMP R1, #2       @Check for "chocolate"
-    BEQ _choc        @If user entered 2 goto _choc
-    CMP R1, #3       @Check for "pretzels"
-    BEQ _pretzel     @If user entered 3 goto _pretzel
-    LDR R0, =msg7    @If we get here user entered
+	CMP	R1, #1
+	MOVEQ	R3, #75
+	CMP	R1, #2
+	MOVEQ	R3, #125
+	CMP	R1, #3
+	MOVEQ	R3, #90
+
+	LDRGT	R0, =msg7
+	BLGT	printf
+	MOVGT	R7, #1
+	SWIGT	#0
+
+	LDRLE	R4, =quantity
+	LDRLE	R4, [R4]
+	MULLE	R1, R3, R4
+	LDRLE	R0, =msg6
+	BLLE	printf
+	MOVLE	R7, #0
+	SWILE	#0
+	
+	
+
+
+@    CMP R1, #1       @Check for "peanuts"
+@    BEQ _peanut      @If user entered 1 goto _peanuts
+@    CMP R1, #2       @Check for "chocolate"
+@    BEQ _choc        @If user entered 2 goto _choc
+@    CMP R1, #3       @Check for "pretzels"
+@    BEQ _pretzel     @If user entered 3 goto _pretzel
+@    LDR R0, =msg7    @If we get here user entered
                      @an illegal selection so print
                      @error message and terminate
-    BL printf
-    MOV R7, #1
-    SWI #0           @Terminate, error condition
-_peanut:
-    MOV R3, #75      @Move 75 cents into R3
-    BAL _compute
-_choc:
-    MOV R3, #125     @Move 125 cents into R3
-    BAL _compute
-_pretzel:
-    MOV R3, #90      @Move 90 cents into R3
-_compute:
-    LDR R4, =quantity @Get address of var quantity
-    LDR R4, [R4]      @Value of quantity now in R4
-    MUL R1, R3, R4    @Multiply number of cents times quantity
-                      @and put result in R1
-    LDR R0, =msg6     @Final message
-    BL printf
-    MOV R7, #0
-    SWI #0        @Normal exit
+@    BL printf
+@    MOV R7, #1
+@    SWI #0           @Terminate, error condition
+@_peanut:
+@    MOV R3, #75      @Move 75 cents into R3
+@    BAL _compute
+@_choc:
+@    MOV R3, #125     @Move 125 cents into R3
+@    BAL _compute
+@_pretzel:
+@    MOV R3, #90      @Move 90 cents into R3
+@_compute:
+@    LDR R4, =quantity @Get address of var quantity
+@    LDR R4, [R4]      @Value of quantity now in R4
+@    MUL R1, R3, R4    @Multiply number of cents times quantity
+@                      @and put result in R1
+@    LDR R0, =msg6     @Final message
+@    BL printf
+@    MOV R7, #0
+@    SWI #0        @Normal exit
 
 .data
 select: .word 0
